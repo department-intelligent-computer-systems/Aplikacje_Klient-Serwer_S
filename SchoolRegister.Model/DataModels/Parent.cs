18bcf1769e84dd8120a2447496b2643a5c29bcbd
Adrian Wasiewicz <tttt@asdfsao2.pl>
2023-03-06 21:16:35 +0100

    lab 1

--- SchoolRegister.Model/DataModels/Parent.cs
+++ SchoolRegister.Model/DataModels/Parent.cs
@@ -1,6 +1,6 @@
 namespace SchoolRegister.Model.DataModels
 {
-    public class Parent
+    public class Parent : User
     {
         public IList<Student> Students {get;set;}
     }
--- SchoolRegister.Model/DataModels/Subject.cs
+++ SchoolRegister.Model/DataModels/Subject.cs
@@ -7,9 +7,8 @@ namespace SchoolRegister.Model.DataModels
         public string Name {get;set;}
         public string Description {get;set;}
         public IList<SubjectGroup> SubjectGroups {get;set;}
-        public string? Teacher {get;set;}
-        public int? TeacherId {get;set;}
-
+        public string Teacher {get;set;}
+        public int TeacherId {get;set;}
         public IList<Grade> Grades {get;set;}
 
     }
