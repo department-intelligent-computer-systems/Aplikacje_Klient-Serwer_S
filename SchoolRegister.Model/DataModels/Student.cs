9fb420bf71dd06b3aa58c0369a30ca4b51e1dc4b
Dawid_Hejduk <dhfasughi2bj3x@asiygb3nl12x.com>
2023-03-08 00:52:08 +0100

    Grades

--- SchoolRegister.Model/DataModels/Student.cs
+++ SchoolRegister.Model/DataModels/Student.cs
@@ -23,7 +23,11 @@ namespace SchoolRegister.Model.DataModels
 
         public IDictionary<string, List<GradeScale>> GradesPerSubject { get; }
 
-        public int test {get;set;}
+        public double AverageGrade => Grades == null || Grades.count == 0 ? 0.0d : Math.Round(Grades.Average(g=> (int)g.GradeValue),1);
+
+        public IDictionary<string,double> AverageGradePerSubject => Grades == null ? new Dictionary<string,double>() : Grades.GroupBy(g=>g.Subject.Name).Select(g=>new {SubjectName = g.Key, AvgGrade = Math.round(g.Average(avg=> (int)avg.GradeValue),1)}).ToDictionary(avg=>avg.SubjectName, avg=>avg.AvgGrade);
+
+        public IDictionary<string, List<GradeScale>> GradesPerSubject => Grades == null ? new Dictionary<string,List<GradeScale>>() : Grades.GroupBy(g=> g.SubjectName).Select(g=> new {SubjectName = g.Key, GradeList = g.Select(x=>x.GradeValue).ToList()}).ToDictionary(x=> x.SubjectName,x=>x.GradeList);
 
         public Student()
         { }
