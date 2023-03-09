3f7b91be5974391bf312ff13dc146dc80735e236
Grzegorz <grzegorz@test.pl>
2023-03-09 19:27:29 +0100

    Lab_1

--- SchoolRegister.Model/DataModels/Student.cs
+++ SchoolRegister.Model/DataModels/Student.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Identity;
+using System.Linq;
 using System;
 namespace SchoolRegister.Model.DataModels
 {
@@ -9,7 +10,7 @@ namespace SchoolRegister.Model.DataModels
         public IList<Grade> Grades { get; set; }
         public Parent Parent { get; set; }
         public int? ParentId { get; set; }
-        public double AverageGrade;
+        public double AverageGrade => Grades == null || Grades.Count == 0 ? 0.0d : Math.Round(Grades.Average(g => (int)g.GradeValue), 1);
         public IDictionary<string, double> AverageGradePerSubject;
         public IDictionary<string, List<GradeScale>> GradesPerSubject;
 
