2150e6b8532ed0b77444f6198e71e65527e9b840
Pawel_Trojnar <p_trojnar@cos.com>
2023-03-13 16:10:40 +0100

    Lab1

--- SchoolRegister.Model/DataModels/Student.cs
+++ SchoolRegister.Model/DataModels/Student.cs
@@ -8,7 +8,7 @@ namespace SchoolRegister.Model.DataModels
         public IList<Grade> Grades { get; set; }
         public Parent Parent { get; set; }
         public int ParentId { get; set; }
-        public double AverageGrade;
+        public double AverageGrade => Grades == null || Grades.Count == 0 ? 0.0d : Math.Round(Grades.Average(g=>(int)g.GradeValue),1);
         public IDictionary<string, double> AverageGradePerSubject;
         public IDictionary<string, List<GradeScale>> GradesPerSubject;
 
