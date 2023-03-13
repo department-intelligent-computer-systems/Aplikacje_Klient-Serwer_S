99f7dcbf77fb0611cb01b890d196e6d4400f224c
Grzegorz_Baryla <grzegorz@test.pl>
2023-03-13 12:42:20 +0100

    Lab_1

--- SchoolRegister.Model/DataModels/Student.cs
+++ SchoolRegister.Model/DataModels/Student.cs
@@ -9,7 +9,7 @@ namespace SchoolRegister.Model.DataModels
         public Group Group { get; set; }
         public int? GroupId { get; set; }
         public IList<Grade> Grades { get; set; }
-        public Parent Parent { get; set; }
+        public Parent Parent { get; set; }ss
         public int? ParentId { get; set; }
         public double AverageGrade => Grades == null || Grades.Count == 0 ? 0.0d : Math.Round(Grades.Average(g => (int)g.GradeValue), 1);
         public IDictionary<string, double> AverageGradePerSubject => Grades == null || Grades.Count == 0 ? new Dictionary<string, double>() :
